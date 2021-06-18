d203afde4d1a70c99a24325925628954b62d3cd5
Anton Kudryashov <amkudr@mail.ru>
2021-06-18 13:07:30 +0300

    Исправил ошибки

--- handlers.py
+++ handlers.py
@@ -1,7 +1,10 @@
-
+import logging
 
 import utils
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+ 
 
-def greet_user(update,context):
-    print("Пользователь найден")
+def greet_user(update, context):
+    
+    logging.info('User /start')
     update.message.reply_text('Привет!')
