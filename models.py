03aaffbe03e2dee6a091b9a1025066938b790c57
Anton Kudryashov <amkudr@mail.ru>
2021-06-20 12:33:49 +0300

    Добавил bacref, relationship и другие небольшие изменения

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column,Integer,String, Date, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
 
 from db import Base, engine
 
@@ -16,7 +17,8 @@ class Car(Base):
     __tablename__ = 'cars'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(User.id), index=True, nullable=False)
+    driver_id = Column(Integer, ForeignKey(User.id), index=True, nullable=False)
+    user = relationship("User", backref="cars")
     model = Column(String)
     year_of_issue = Column(String)
 
@@ -28,18 +30,26 @@ class Trip(Base):
 
     id = Column(Integer, primary_key=True)
     car_id = Column(Integer, ForeignKey(Car.id), index=True, nullable=False)
+    car = relationship("Car", backref="trips")
     route = Column(String)
     date = Column(Date)
 
     def __repr__(self):
         return f'Trip id: {self.id}'
 
-class Trip_user(Base):
+class User_trip(Base):
     __tablename__ = 'trip_user'
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey(User.id), index=True, nullable=False)
-    car_id = Column(Integer, ForeignKey(Car.id), index=True, nullable=False)
+    user = relationship("User", backref="user_trips")
+    trip_id = Column(Integer, ForeignKey(Trip.id), index=True, nullable=False)
+    trip = relationship("Trip", backref="user_trips")
 
     def __repr__(self):
         return f'Trip_user id: {self.id}'
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine)
+
