4bfb1a01d11abce0319a36e77e70e41d17a331e5
Anton Kudryashov <amkudr@mail.ru>
2021-06-21 15:35:07 +0300

    Добавил связь между таблицами

--- models.py
+++ models.py
@@ -1,7 +1,19 @@
 from sqlalchemy import Column,Integer,String, Date, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Table
 
 from db import Base, engine
+
+
+# user_trips = UserTrip('user_trip',
+#         Column('user_id', Integer, ForeignKey('users.id')),
+#         Column('trip_id', Integer, ForeignKey('trips.id'))
+# )
+
+user_trips = Table('user_trips', Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('trip_id', Integer, ForeignKey('trips.id'))
+)
 
 class User(Base):
     __tablename__ = 'users'
@@ -9,6 +21,8 @@ class User(Base):
     name = Column(String)    
     phone_number = Column(String)    
     subscribe = Column(Boolean)
+    trips = relationship("Trip", secondary = user_trips) #Many to many with Trip
+    car = relationship("Car", backref="driver") #One to many with Car
 
     def __repr__(self):
         return f'User id: {self.id}, name: {self.name}'
@@ -17,37 +31,36 @@ class Car(Base):
     __tablename__ = 'cars'
 
     id = Column(Integer, primary_key=True)
-    driver_id = Column(Integer, ForeignKey(User.id), index=True, nullable=False)
-    user = relationship("User", backref="cars")
+    driver = Column(Integer, ForeignKey(User.id), index=True, nullable=False)    
     model = Column(String)
     year_of_issue = Column(String)
+    trip = relationship("Trip", back_populates="car", uselist=False) #One to one with Trip.car
 
     def __repr__(self):
         return f'Car id: {self.id}'
 
 class Trip(Base):
-    __tablename__ = 'trip'
+    __tablename__ = 'trips'
 
     id = Column(Integer, primary_key=True)
-    car_id = Column(Integer, ForeignKey(Car.id), index=True, nullable=False)
-    car = relationship("Car", backref="trips")
+    car_id = Column(Integer, ForeignKey(Car.id), index=True, nullable=False)    
     route = Column(String)
     date = Column(Date)
+    car = relationship("Car", back_populates="trip", uselist=False) #One to one with Car.trip
+    passengers = relationship("User", secondary = user_trips) #Many to many with User
 
     def __repr__(self):
         return f'Trip id: {self.id}'
 
-class User_trip(Base):
-    __tablename__ = 'trip_user'
+# class UserTrip(Base):
+#     __tablename__ = 'user_trip'
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(User.id), index=True, nullable=False)
-    user = relationship("User", backref="user_trips")
-    trip_id = Column(Integer, ForeignKey(Trip.id), index=True, nullable=False)
-    trip = relationship("Trip", backref="user_trips")
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey(User.id), index=True, nullable=False)    
+#     trip_id = Column(Integer, ForeignKey(Trip.id), index=True, nullable=False)
 
-    def __repr__(self):
-        return f'Trip_user id: {self.id}'
+#     def __repr__(self):
+#         return f'Trip_user id: {self.id}'
 
 
 if __name__ == '__main__':
