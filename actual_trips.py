4f01806ef2621b1c5061f021a1cc75ba8b61c018
Anton Kudryashov <amkudr@mail.ru>
2021-07-13 21:15:47 +0300

    Вынес логику из f строк

--- actual_trips.py
+++ actual_trips.py
@@ -9,12 +9,20 @@ def actual_trips(update, context):
     user_text = "<b>Актуальные поездки:</b>"
     i = 1
     for trip in request:
+        departure_point = trip.departure_point
+        arrival_point = trip.arrival_point
+        time = trip.date.time().strftime('%H:%M')
+        date = trip.date.date().strftime('%d/%m/%y')
+        driver_name = trip.car.driver.name
+        driver_phone = trip.car.driver.phone
+        car_model = trip.car.model
+        car_year = trip.car.year
         user_text += f"""
-{i}) <b>{trip.departure_point} - {trip.arrival_point}</b>
-     <b>Время:</b> {trip.date.time().strftime('%H:%M')} <b>Дата:</b> {trip.date.date().strftime('%d/%m/%y')}
-     <b>Водитель:</b> {trip.car.driver.name}
-     <b>Машина:</b> {trip.car.model}, {trip.car.year} года выпуска
-     <b>Телефон водителя:</b> {trip.car.driver.phone}
+{i}) <b>{departure_point} - {arrival_point}</b>
+     <b>Время:</b> {time} <b>Дата:</b> {date}
+     <b>Водитель:</b> {driver_name}
+     <b>Машина:</b> {car_model}, {car_year} года выпуска
+     <b>Телефон водителя:</b> {driver_phone}
     """
         i += 1
     update.message.reply_text(user_text, parse_mode=ParseMode.HTML)
